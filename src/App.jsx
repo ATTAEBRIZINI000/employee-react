8ca292476594c5adb5c61312224ed756e103e9e2
taebrizini <xovangpa@gmail.com>
2023-08-04 00:19:12 +0200

    this is my first react app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-
-
+import Employee from "./components/employee";
 import './App.css'
-
+import {useState} from "react";
 function App() {
-  
+  const [role , setRole] = useState("dev");
+   
+
+  const ShowEmployees =true ;
   return (
-    <div>
-      
+
+    <div className = "App">
+        
+       
+        {ShowEmployees?
+     <>
+     <input type = "text" onChange = {(e) => {
+        console.log(e.target.value);
+        setRole(e.target.value);
+
+
+     }}/>
+      <Employee name="Caleb" role = "WebDev"/>
+      <Employee name = "Suzan" role = {role}/>
+      <Employee name = "Attae"/>
+  
+      </>
+      :
+      <p>You can not see the employees</p>
+      }
     </div>
   )
 }
